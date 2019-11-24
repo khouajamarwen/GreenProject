3b131ea415ae1d810ad862d9b04add0bb4d8af91
marwen <khouajamarwen@gmail.com>
2019-11-24 18:29:08 +0100

    Commit 24-11-2019

---
+++ GreenWeb/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreenWeb.Models;
+
+namespace GreenWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+            
+        public IActionResult Detail(int id)
+        {
+            var aSac = new Sac();
+            aSac.name = "FirstSac";
+            return View(aSac);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GreenWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreenWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
